05839b9bed85807a833dd08d35691e75288e7abd
LucasHdS <lucasnsv@gmail.com>
2019-03-25 01:29:55 -0300

    adicionada funcionalidade para upload de imagens para amazaons s3

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,36 @@
 const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
+const multerS3 = require('multer-s3');
+const Post = require('./config/models/post');
 
-routes.get('/',(req,res) =>{
-    return res.json({'Hello': 'World'});
+routes.get('/posts', async (req,res) =>{
+    const posts = await Post.find();
+
+    return res.json(posts); 
 });
 
-routes.post('/posts', multer(multerConfig).single('file'), (req,res) =>{
+routes.post('/posts', multer(multerConfig).single('file'), async (req,res) =>{
     
-    console.log(req.file);
+    const { originalname: name, size, key, location: url = '' } = req.file;
 
-    return res.json({'Hello': 'World'});
+    const post = await Post.create({
+        name,
+        size, 
+        key,
+        url,
+        location: ''
+    });
+
+    return res.json(post);
+});
+
+routes.delete('/posts/:id', async( req, res) =>{
+    const post = await Post.findById(req.params.id);
+
+    await post.remove();
+
+    return res.send();
 });
 
 module.exports = routes;
